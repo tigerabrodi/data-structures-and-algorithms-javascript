c930257644b3e810e1f7b34c13af0a143f2300b0
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-10 16:57:52 +0100

    array index

---
+++ src/arrays/index.js
@@ -0,0 +1,6 @@
+const arrWithNumbers = [1, 2, 3, 4, 5]
+
+arrWithNumbers.pop() // [1, 2, 3, 4]
+arrWithNumbers.push(6) // [1, 2, 3, 4, 6]
+arrWithNumbers.shift() // [2, 3, 4, 6]
+arrWithNumbers.unshift(1) // [1, 2, 3, 4, 6]
