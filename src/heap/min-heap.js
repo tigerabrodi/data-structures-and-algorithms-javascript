f9324bf98fc88de167aa4e9e79e7a0671f306f58
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-12 16:25:03 +0100

    is heap empty

---
+++ src/heap/min-heap.js
@@ -0,0 +1,9 @@
+export class MinHeap {
+  constructor() {
+    this.heap = []
+  }
+
+  isEmpty() {
+    return this.heap.length === 0
+  }
+}
---
+++ src/heap/min-heap.test.js
@@ -0,0 +1,36 @@
+import { it, expect } from 'vitest'
+import { MinHeap } from './min-heap'
+
+// Test for creating a new min heap
+it.only('should create an empty min heap', () => {
+  const minHeap = new MinHeap()
+  expect(minHeap).toBeDefined()
+  expect(minHeap.isEmpty()).toBe(true)
+})
+
+// Test for inserting elements
+it('should insert elements and maintain min heap property', () => {
+  const minHeap = new MinHeap()
+  minHeap.insert(3)
+  minHeap.insert(1)
+  minHeap.insert(6)
+
+  expect(minHeap.peek()).toBe(1)
+})
+
+// Test for removing the root element
+it('should remove the root element and maintain min heap property', () => {
+  const minHeap = new MinHeap()
+  minHeap.insert(3)
+  minHeap.insert(1)
+  minHeap.insert(6)
+  expect(minHeap.remove()).toBe(1)
+  expect(minHeap.peek()).toBe(3)
+})
+
+// Test for heapify
+it('should correctly heapify an array', () => {
+  const minHeap = new MinHeap()
+  minHeap.heapify([3, 1, 6])
+  expect(minHeap.peek()).toBe(1)
+})
