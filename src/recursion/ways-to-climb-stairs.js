cb98039c315f2418b32c56c3859cb9d28ea6803d
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-19 14:29:33 +0100

    memoization

--- src/recursion/ways-to-climb-stairs.js
+++ src/recursion/ways-to-climb-stairs.js
@@ -11,3 +11,32 @@ export function waysToClimbStairs(target, possibleSteps) {
 
   return nbWays
 }
+
+export function waysToClimbStairsWithMemoization(
+  target,
+  possibleSteps,
+  memoization
+) {
+  if (target === 0) return 1
+
+  // If we already computed the number of ways to climb the stairs for the target, we return it
+  // There is no need to recompute it
+  if (memoization[target]) return memoization[target]
+
+  let nbWays = 0
+
+  // Once this process is done, we store the result in the memoization object
+  possibleSteps.forEach((step) => {
+    if (target - step >= 0) {
+      nbWays += waysToClimbStairsWithMemoization(
+        target - step,
+        possibleSteps,
+        memoization
+      )
+    }
+  })
+
+  memoization[target] = nbWays
+
+  return nbWays
+}
