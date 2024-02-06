d7367605d486050f7b36bed2aa641fdf219b93f6
Tiger Abrodi <tigerabrodi@gmail.com>
2024-02-06 11:55:09 +0100

    Add reverseList function to reverse a singly-linked list

---
+++ src/leetcode/easy/linked-lists/reverseList.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let prev = null
+  let current = head
+
+  while (current !== null) {
+    let temp = current.next
+    current.next = prev
+    prev = current
+    current = temp
+  }
+
+  return prev
+}
