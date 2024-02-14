cb6aadd6a62790f2395aae6d22832ad356e31892
Tiger Abrodi <tigerabrodi@gmail.com>
2024-02-14 15:43:55 +0100

    Add insertIntoBST function to handle binary tree node insertion

---
+++ src/leetcode/trees/insertIntoBST.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function (root, val) {
+  if (!root) {
+    return new TreeNode(val)
+  }
+
+  if (root.val > val) {
+    root.left = insertIntoBST(root.left, val)
+  } else {
+    root.right = insertIntoBST(root.right, val)
+  }
+
+  return root
+}
