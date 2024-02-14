fce817a1cdf9065b763c56925acadedb4a4bedf9
Tiger Abrodi <tigerabrodi@gmail.com>
2024-02-14 15:43:52 +0100

    Add inorderTraversal function to perform inorder traversal of a binary tree

---
+++ src/leetcode/trees/inorderTraversal.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root, res = []) {
+  if (root) {
+    inorderTraversal(root.left, res)
+    res.push(root.val)
+    inorderTraversal(root.right, res)
+  }
+
+  return res
+}
