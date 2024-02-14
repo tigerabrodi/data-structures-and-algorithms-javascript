ddcc0fce2d976b3dede53aa97017909e1be6fd69
Tiger Abrodi <tigerabrodi@gmail.com>
2024-02-14 17:23:11 +0100

    Add levelOrder function for binary tree traversal

---
+++ src/leetcode/trees/levelOrder.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  if (!root) return []
+
+  const result = []
+  const queue = [root]
+
+  while (queue.length) {
+    let levelLength = queue.length
+    const currentLevel = []
+
+    for (let i = 0; i < levelLength; i++) {
+      const node = queue.shift()
+      currentLevel.push(node.val)
+
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+
+    result.push(currentLevel)
+  }
+
+  return result
+}
