bf16c42d14f2a72317b36644f9c18f8a6d2a551c
Tiger Abrodi <tigerabrodi@gmail.com>
2024-02-14 15:43:45 +0100

    Add buildTree function to create a binary tree from preorder and inorder arrays

---
+++ src/leetcode/trees/buildTree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {
+  if (preorder.length === 0 || inorder.length === 0) {
+    return null
+  }
+
+  let value = preorder[0]
+  let root = new TreeNode(value)
+
+  let mid = inorder.indexOf(value)
+
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid))
+  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1))
+
+  return root
+}
