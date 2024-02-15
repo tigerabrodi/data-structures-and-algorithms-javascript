a7bc82ed9dc744ac18703481538ab3edb6f55d2f
Tiger Abrodi <tigerabrodi@gmail.com>
2024-02-15 17:10:16 +0100

    Add subsets.js with backtracking implementation

---
+++ src/leetcode/backtracking/subsets.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums) {
+  let res = []
+  let subset = []
+
+  function dfs(i) {
+    if (i >= nums.length) {
+      res.push(subset.slice())
+      return
+    }
+
+    subset.push(nums[i])
+    dfs(i + 1)
+
+    subset.pop()
+    dfs(i + 1)
+  }
+
+  dfs(0)
+  return res
+}
