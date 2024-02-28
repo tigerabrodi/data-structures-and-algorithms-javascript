b447fb45d6270821834dbd9ac27449c1a358f4b2
Tiger Abrodi <tigerabrodi@gmail.com>
2024-02-28 15:12:08 +0100

    Refactor Fibonacci function

--- src/leetcode/dynamic-programming/fib.js
+++ src/leetcode/dynamic-programming/fib.js
@@ -2,14 +2,34 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function (n, cache = {}) {
-  if (n < 2) return n
+// var fib = function (n, cache = {}) {
+//   if (n < 2) return n
 
-  if (cache[n] !== undefined) {
-    return cache[n]
-  }
+//   if (cache[n] !== undefined) {
+//     return cache[n]
+//   }
 
-  cache[n] = fib(n - 1) + fib(n - 2)
+//   cache[n] = fib(n - 1) + fib(n - 2)
 
-  return cache[n]
-}
+//   return cache[n]
+// }
+
+// /**
+//  * @param {number} n
+//  * @return {number}
+//  */
+// var fib = function(n) {
+//     if (n < 2) return n
+
+//     let arr = [0, 1]
+//     let i = 2
+
+//     while (i <= n) {
+//         let temp = arr[1]
+//         arr[1] = arr[0] + arr[1]
+//         arr[0] = temp
+//         i++
+//     }
+
+//     return arr[1]
+// };
