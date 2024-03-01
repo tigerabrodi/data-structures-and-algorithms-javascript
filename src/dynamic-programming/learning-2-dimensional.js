7c004c5f89ab08eacdf1db42649955029fb9bf04
Tiger Abrodi <tigerabrodi@gmail.com>
2024-03-01 16:40:00 +0100

    Add tabulation approach for 2-dimensional learning

--- src/dynamic-programming/learning-2-dimensional.js
+++ src/dynamic-programming/learning-2-dimensional.js
@@ -40,3 +40,20 @@ function withMemoization(r, c, rows, cols, memo) {
 
   return memo[r][c]
 }
+
+function withTabulation(rows, cols) {
+  let prevRow = Array(cols).fill(0)
+
+  for (let row = rows - 1; row >= 0; row--) {
+    let currentRow = Array(cols).fill(0)
+    currentRow[cols - 1] = 1
+
+    for (let col = cols - 2; col >= 0; col--) {
+      currentRow[col] = currentRow[col + 1] + prevRow[col]
+    }
+
+    prevRow = currentRow
+  }
+
+  return prevRow[0]
+}
