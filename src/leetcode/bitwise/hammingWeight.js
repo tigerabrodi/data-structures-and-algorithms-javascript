eaebc93d538f6d7478feed7d39fcc2646400c665
Tiger Abrodi <tigerabrodi@gmail.com>
2024-03-02 17:10:35 +0100

    Refactored hammingWeight function to use modulo operator for counting bits

--- src/leetcode/bitwise/hammingWeight.js
+++ src/leetcode/bitwise/hammingWeight.js
@@ -1,3 +1,22 @@
+// /**
+//  * @param {number} n - a positive integer
+//  * @return {number}
+//  */
+// var hammingWeight = function (n) {
+//   let count = 0
+
+//   while (n > 0) {
+//     // Use !== 0 to handle negative numbers correctly
+//     if ((n & 1) === 1) {
+//       count += 1
+//     }
+
+//     n = n >>> 1 // Use unsigned right shift
+//   }
+
+//   return count
+// }
+
 /**
  * @param {number} n - a positive integer
  * @return {number}
@@ -6,11 +25,10 @@ var hammingWeight = function (n) {
   let count = 0
 
   while (n > 0) {
-    // Use !== 0 to handle negative numbers correctly
-    if ((n & 1) === 1) {
-      count += 1
-    }
-
+    // This solution uses the modulo operator
+    // For example 101 % 2 = 1 -> The last digit is 1
+    // 100 % 2 = 0 -> The last digit is 0
+    count += n % 2
     n = n >>> 1 // Use unsigned right shift
   }
 
