aef1324aafed4008ced0f4ce58aeb929570a628f
Tiger Abrodi <tigerabrodi@gmail.com>
2024-03-04 15:46:03 +0100

    Add removeDuplicates function to remove duplicates from an array

---
+++ src/leetcode/arrays/removeDuplicates.js
@@ -0,0 +1,18 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var removeDuplicates = function (nums) {
+//   let R = 1
+//   let L = 0
+
+//   while (R < nums.length) {
+//     if (nums[L] === nums[R]) {
+//       nums.splice(R, 1)
+//       continue
+//     }
+
+//     L++
+//     R++
+//   }
+// }
