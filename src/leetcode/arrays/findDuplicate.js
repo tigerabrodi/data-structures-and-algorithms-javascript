9ae252cd71fbdd90e7b16ef3eecfeb84ef16c473
Tiger Abrodi <tigerabrodi@gmail.com>
2024-03-06 15:45:54 +0100

    Refactor findDuplicate function to use Floyd's Tortoise and Hare algorithm

--- src/leetcode/arrays/findDuplicate.js
+++ src/leetcode/arrays/findDuplicate.js
@@ -1,15 +1,34 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var findDuplicate = function (nums) {
-  const set = new Set()
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var findDuplicate = function (nums) {
+//   const set = new Set()
 
-  for (let i = 0; i < nums.length; i++) {
-    if (set.has(nums[i])) {
-      return nums[i]
-    }
+//   for (let i = 0; i < nums.length; i++) {
+//     if (set.has(nums[i])) {
+//       return nums[i]
+//     }
 
-    set.add(nums[i])
+//     set.add(nums[i])
+//   }
+// }
+
+function findDuplicate(nums) {
+  let slow = 0
+  let fast = 0
+
+  do {
+    slow = nums[slow]
+    fast = nums[nums[fast]]
+  } while (slow !== fast)
+
+  let slow2 = 0
+
+  while (slow !== slow2) {
+    slow = nums[slow]
+    slow2 = nums[slow2]
   }
+
+  return slow
 }
