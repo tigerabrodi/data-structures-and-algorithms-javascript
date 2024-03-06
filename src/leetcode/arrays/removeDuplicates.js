0d13632a329f768758421624c6d999322a464c71
Tiger Abrodi <tigerabrodi@gmail.com>
2024-03-06 15:46:15 +0100

    Refactor removeDuplicates function

--- src/leetcode/arrays/removeDuplicates.js
+++ src/leetcode/arrays/removeDuplicates.js
@@ -1,18 +1,18 @@
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-// var removeDuplicates = function (nums) {
-//   let R = 1
-//   let L = 0
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let R = 1
+  let L = 0
 
-//   while (R < nums.length) {
-//     if (nums[L] === nums[R]) {
-//       nums.splice(R, 1)
-//       continue
-//     }
+  while (R < nums.length) {
+    if (nums[L] === nums[R]) {
+      nums.splice(R, 1)
+      continue
+    }
 
-//     L++
-//     R++
-//   }
-// }
+    L++
+    R++
+  }
+}
