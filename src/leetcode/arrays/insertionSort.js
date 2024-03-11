88e0d8cd4395a921b8984288dcdf8d0f2c75786b
Tiger Abrodi <tigerabrodi@gmail.com>
2024-03-11 19:39:21 +0100

    Refactor insertion sort algorithm

--- src/leetcode/arrays/insertionSort.js
+++ src/leetcode/arrays/insertionSort.js
@@ -6,11 +6,12 @@ var sortArray = function (nums) {
   for (let i = 1; i < nums.length; i++) {
     let j = i - 1
 
-    while (j >= 0 && nums[j + 1] < nums[j]) {
-      let temp = nums[j + 1]
+    while (j >= 0 && nums[j] > nums[j + 1]) {
+      let tmp = nums[j + 1]
       nums[j + 1] = nums[j]
-      nums[j] = temp
-      j -= 1
+
+      nums[j] = tmp
+      j--
     }
   }
 
